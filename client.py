287ad93764fbca78d406e9534be17518c6a1caab
LuaanNguyen <luanthiennguyen2003@gmail.com>
2025-02-16 17:23:41 -0700

    Add Comments

--- client.py
+++ client.py
@@ -1,31 +1,43 @@
+"""
+Luan Nguyen
+CSE434 
+Homework 01
+Sunday, Feb 16, 2025
+"""
 import socket
 
 port = 12345 # Must have the same port address with server
 ip_address = 'localhost'
 
 # ------------- START CLIENT -------------
-try:
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((ip_address, port))
 
-    welcome_message = client_socket.recv(1024).decode()
-    print(f"Message from Server: {welcome_message}")
+#AF_INET tells Python to use IPv4.
+#SOCK_STREAM tells Python to use the TCP protocol 
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    while True:
-        client_message = input("Enter your message: ")
+# Connect to server
+client_socket.connect((ip_address, port))
 
-        if client_message.lower() == "exit":
-            client_socket.send(client_message.encode())  
-            break
+# Receive a welcome message from server
+welcome_message = client_socket.recv(1024).decode()
+print(f"Message from Server: {welcome_message}")
 
-        client_socket.send(client_message.encode())
+# This loop is intended to keep getting messages to send to server
+while True:
+    client_message = input("Enter your message: ")
 
-        server_message = client_socket.recv(1024).decode()
-        print(f"Message from Server: {server_message}")
-        
-except ConnectionRefusedError:
-    print("Error: Unable to connect to Server.")
+    # Break out of loop if there's an exit message
+    if client_message.lower() == "exit":
+        client_socket.send(client_message.encode())  
+        break
 
-finally:
-    client_socket.close()
-    print("Client Closed.\n")
+    # Send message
+    client_socket.send(client_message.encode())
+
+    # Receive response from server
+    server_message = client_socket.recv(1024).decode()
+    print(f"Message from Server: {server_message}")
+    
+# Close the client socket after the communication is done
+client_socket.close()
+print("Client Closed.\n")
--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+"""
+Luan Nguyen
+CSE434 
+Homework 01
+Sunday, Feb 16, 2025
+"""
 import socket
 import threading
 from datetime import datetime
@@ -8,51 +14,60 @@ ip_address = 'localhost'
 
 # ------------- FUNCTION TO HANDLE CLIENT MESSAGES -------------
 def handle_client_messages(client_socket, client_address):
-    current_time = datetime.now()
+    current_time = datetime.now() #get current time
     
-    message = f"Hello, Client {client_address[1]}!"
+    # Greet the client with a welcome message (e.g., "Hello, Client X!")
+    message = f"Hello, Client {client_address[1]}!" 
+    encoded_message = message.encode('utf-8') #encode the message to send
+
+    client_socket.sendall(encoded_message)
     
-    client_socket.sendall(message.encode('utf-8'))
+    # Log interaction to the console.
     print(f"[{current_time.strftime('%a %d %b %Y, %I:%M%p')}] Connection established with Client {client_address[1]}")
 
-    try:
-        while True:
-            client_message = client_socket.recv(1024)
-            client_message_decoded =  client_message.decode()
-            if not client_message_decoded:
-                break  
+    # Loop to keep receving message from client
+    while True:
+        # Receive message from client and decode it
+        client_message = client_socket.recv(1024)
+        client_message_decoded =  client_message.decode()
+        
+        if not client_message_decoded:
+            break  
 
-            current_time = datetime.now()
-            print(f"[{current_time.strftime('%a %d %b %Y, %I:%M%p')}] Client {client_address[1]}'s message: {client_message_decoded}")
+        #Get the current time and format it appropriately (documentation: https://docs.python.org/3/library/datetime.html)
+        current_time = datetime.now()
+        
+        # Log all interactions (e.g., timestamps and messages) to the console.
+        print(f"[{current_time.strftime('%a %d %b %Y, %I:%M%p')}] Client {client_address[1]}'s message: {client_message_decoded}")
 
-            response = f"Received: {client_message_decoded}"
-            client_socket.sendall(response.encode('utf-8'))
+        # Accept and echo any messages received from the client.
+        response = f"Received: {client_message_decoded}"
+        client_socket.sendall(response.encode('utf-8'))
 
-    except ConnectionResetError:
-        print(f"Client {client_address[1]} disconnected unexpectedly.")
-
-    finally:
-        client_socket.close()
-        print(f"[{datetime.now().strftime('%a %d %b %Y, %I:%M%p')}] Connection with Client {client_address[1]} is closed.")
+    # Close client socket when done and log the message that client is closing.
+    client_socket.close()
+    print(f"[{datetime.now().strftime('%a %d %b %Y, %I:%M%p')}] Connection with Client {client_address[1]} is closed.")
 
 # ------------- START SERVER -------------
-try:
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    server_socket.bind((ip_address, port))
-    
-    server_socket.listen(maximum_clients)
 
-    print(f"Server is listening on {ip_address}:{port}...")
+#AF_INET tells Python to use IPv4.
+#SOCK_STREAM tells Python to use the TCP protocol 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    while True:
-        client_socket, client_address = server_socket.accept()
-        client_thread = threading.Thread(target=handle_client_messages, args=(client_socket, client_address))
-        client_thread.start()
+# Bind the socket to a specific IP and port 
+server_socket.bind((ip_address, port))
 
-except KeyboardInterrupt:
-    print("Keyboard Interupted, Server shutting down...\n")
+# Listen for incoming connections with maximum amount of clients (10) at a time
+server_socket.listen(maximum_clients)
 
-finally:
-    server_socket.close()
-    print("Server Closed.\n")
+print(f"Server is listening on {ip_address}:{port}...")
+
+# Loop to handle multiple clients using Threading
+while True:
+    client_socket, client_address = server_socket.accept()
+    client_thread = threading.Thread(target=handle_client_messages, args=(client_socket, client_address))
+    client_thread.start()
+
+# Close the server socket after the communication is done
+server_socket.close()
+print("Server Closed.\n")
